933843f304d7ace0cd172fa77e750890f6a48ac0
Rafael Lara <rafaelsilva.l1995@gmail.com>
2021-09-04 15:19:22 -0300

    Initial Commit

---
+++ MyStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyStore
+//
+//  Created by Rafael Silva on 04/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyStore/MyStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoreApp.swift
+//  MyStore
+//
+//  Created by Rafael Silva on 04/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MyStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
